ca72ef1535d3a8bcc0365aef9a56b3c33e2284f1
James Galantino <james_galantino@aol.com>
2021-02-03 15:25:13 -0500

    Imported generateMarkdown.js, replaced index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// DEPENDENCIES
+// Include packages needed for this application
+
+// DATA
+
+// USER INTERACTIONS
+// Create an array of questions for user input
+const questions = [];
+
+// FUNCTIONS
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
