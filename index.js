85a62804087194ab7750ec79dfd59b19156e24b9
James Galantino <james_galantino@aol.com>
2021-02-03 20:01:58 -0500

    Added prompts to inquirer array

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-// DEPENDENCIES
+// DEPENDENCIES ===============================
 // Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// DATA
+// DATA =======================================
 
-// USER INTERACTIONS
+// USER INTERACTIONS ==========================
 // Create an array of questions for user input
-const questions = [];
+const questions = inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            name: "username",
+                            message: "What is your username?",
+                        },
+                        {
+                            type: "input",
+                            name: "description",
+                            message: "Describe your application with a short description.",
+                        },
+                        {
+                            type: "confirm",
+                            name: "tableContents",
+                            message: "Would you like to include a Table of Contents?",
+                        },
+                    ])
+                    // callback function for user responses
+                    .then(response => {
+                        console.log(response.username);
+                        console.log(response.description);
+                        console.log(response.tableContents);
+                    })
 
-// FUNCTIONS
+// FUNCTIONS ==================================
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     // console.log(data)
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
